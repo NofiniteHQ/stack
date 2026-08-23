0b0f18bab9b7f668eeab7710eb44cea87c496d4d
Suyash Das <71306286+suyashdas@users.noreply.github.com>
2026-08-23 23:10:43 +0530

    chore: remove scratch files

--- fix-ci.py
+++
@@ -1,9 +0,0 @@
-﻿# -*- coding: utf-8 -*-
-with open(".github/workflows/ci.yml", "r", encoding="utf-8") as f:
-    t = f.read()
-
-t = t.replace("pnpm nx run-many -t test", "pnpm nx affected -t test --exclude nui")
-t = t.replace("pnpm nx run-many -t build", "pnpm nx affected -t build")
-
-with open(".github/workflows/ci.yml", "w", encoding="utf-8") as f:
-    f.write(t)
